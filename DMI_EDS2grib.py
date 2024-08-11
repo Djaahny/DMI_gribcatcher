722f128077b902c00b51c0533efb1dcd30ea3cef
Djaahny <157282710+Djaahny@users.noreply.github.com>
2024-08-11 11:24:36 +0200

    Update DMI_EDS2grib.py
    release 1.0

--- DMI_EDS2grib.py
+++ DMI_EDS2grib.py
@@ -6,14 +6,17 @@ import argparse
 # Step 1: Set up argparse to handle command-line arguments
 parser = argparse.ArgumentParser(description="Download GeoJSON data and convert to GRIB2.")
 parser.add_argument("--api-key", required=True, help="API key for accessing the weather data.")
+parser.add_argument("--bbox", required=False,default="9.29,54.54,13.04,56.19", help="Bounding box of area to download - default is: 9.29,54.54,13.04,56.19(inner inner dansih waters).")
 args = parser.parse_args()
+
+print(args.bbox)
 
 # Step 2: Generate the dynamic datetime for the URL (current time UTC minus 1 hour)
 current_time_utc_minus_1_hour = datetime.now(timezone.utc) - timedelta(hours=1)
 datetime_string = current_time_utc_minus_1_hour.strftime("%Y-%m-%dT%H:%M:%S") + "Z"
 
 # Update the URL with the dynamic datetime and API key
-url = f"https://dmigw.govcloud.dk/v1/forecastedr/collections/dkss_nsbs/cube?bbox=9.29,54.54,13.04,56.19&parameter-name=wind-u,wind-v&datetime={datetime_string}%2F..&crs=crs84&f=GeoJSON&api-key={args.api_key}"
+url = f"https://dmigw.govcloud.dk/v1/forecastedr/collections/dkss_nsbs/cube?bbox={args.bbox}&parameter-name=wind-u,wind-v&datetime={datetime_string}%2F..&crs=crs84&f=GeoJSON&api-key={args.api_key}"
 
 print(url)
 
@@ -45,7 +48,7 @@ if geojson_data:
         data_by_step[step_time]["wind_u"].append(wind_u_value if wind_u_value is not None else 0.0)
         data_by_step[step_time]["wind_v"].append(wind_v_value if wind_v_value is not None else 0.0)
 
-    i = 0
+
     # Step 5: Create a GRIB2 file with steps
     with open("output_with_steps.grib2", "wb") as grib_file:
         for step_time, data in data_by_step.items():
@@ -53,12 +56,9 @@ if geojson_data:
             forecast_hour = int((forecast_time - datetime(2024, 8, 10, 0, 0, tzinfo=timezone.utc)).total_seconds() / 3600)
             #print(forecast_time)
             dataDate = int(forecast_time.strftime("%Y%m%d"))
-            refTime = (forecast_time.strftime("%H%M").zfill(4))
-            date_time_int = int(refTime)
-            forecastTime = i
-            i = i + 1
-            print(str(dataDate) )#+ " " + str(forecastTime))
-            print(int(refTime))
+            refTime = int(forecast_time.strftime("%H%M"))          
+            #print(str(dataDate) )#+ " " + str(forecastTime))
+            #print(int(refTime))
 
             # Creating wind-u field
             gid_u = ec.codes_grib_new_from_samples("regular_ll_sfc_grib2")
@@ -66,8 +66,8 @@ if geojson_data:
             ec.codes_set(gid_u, "parameterCategory", 2)  # Momentum
             ec.codes_set(gid_u, "parameterNumber", 2)  # u-component of wind
             ec.codes_set(gid_u, "dataDate", dataDate)  # Set the date of the forecast
-            ec.codes_set(gid_u, "forecastTime", forecastTime)  # Set forecast hour
-            #ec.codes_set(gid_u, "dataTime", 1) #Set the data time
+            #ec.codes_set(gid_u, "forecastTime", forecastTime)  # Set forecast hour
+            ec.codes_set(gid_u, "dataTime", refTime) #Set the data time
             ec.codes_set_values(gid_u, data["wind_u"])
             ec.codes_set(gid_u, "latitudeOfFirstGridPointInDegrees", min(data["lats"]))
             ec.codes_set(gid_u, "longitudeOfFirstGridPointInDegrees", min(data["lons"]))
@@ -85,8 +85,8 @@ if geojson_data:
             ec.codes_set(gid_v, "parameterCategory", 2)  # Momentum
             ec.codes_set(gid_v, "parameterNumber", 3)  # v-component of wind
             ec.codes_set(gid_v, "dataDate", dataDate)  # Set the date of the forecast
-            ec.codes_set(gid_v, "forecastTime", forecastTime)  # Set forecast hour
-            #ec.codes_set(gid_u, "dataTime", 1) #Set the data time
+            #ec.codes_set(gid_v, "forecastTime", forecastTime)  # Set forecast hour
+            ec.codes_set(gid_u, "dataTime", refTime) #Set the data time
             ec.codes_set_values(gid_v, data["wind_v"])
             ec.codes_set(gid_v, "latitudeOfFirstGridPointInDegrees", min(data["lats"]))
             ec.codes_set(gid_v, "longitudeOfFirstGridPointInDegrees", min(data["lons"]))
